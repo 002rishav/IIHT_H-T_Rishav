9957dd8d9ba6b770fc0cae36106ff75086938c15
unknown <cogjava4585@iiht.tech>
2023-01-16 23:56:59 +0530

    added merchant microservice

--- Use Case 2/Product Mgmt Portal/User-Service/src/main/java/com/user/MerchantServiceApplication.java
+++ Use Case 2/Product Mgmt Portal/User-Service/src/main/java/com/user/MerchantServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class MerchantServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(MerchantServiceApplication.class, args);
     }
     @Bean
     public RestTemplate restTemplate(){
--- Use Case 2/Product Mgmt Portal/User-Service/src/main/resources/create_subscription_detail_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE if not exists user_service.subscription_details (
-	id int primary key auto_increment,
-    book_id varchar(255) not null,
-    user_name varchar(255) not null,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp,
-    CONSTRAINT UNIQUE(book_id,user_name)
-    );
