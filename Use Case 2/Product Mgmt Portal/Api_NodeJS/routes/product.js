8c13fc3934691b62e94668d6c87be28aec6a7e60
unknown <cogjava4585@iiht.tech>
2023-01-23 13:36:45 +0530

    added changes to nodejs api

--- Use Case 2/Product Mgmt Portal/Api_NodeJS/routes/product.js
+++ Use Case 2/Product Mgmt Portal/Api_NodeJS/routes/product.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const router = express.Router()
-// const Product = require('../models/product')
+ const Product = require('../models/product')
 
 router.get('/', async(req,res) => {
     try{
-        //    const product = await Product.find()
+        const product = await Product.find()
+        res.json(product)
+    }catch(err){
+        res.send('Error ' + err)
+    }
+})
+
+router.get('/:id', async(req,res) => {
+    try{
+           const product = await Product.findById(req.params.id)
            res.json(product)
     }catch(err){
         res.send('Error ' + err)
+    }
+})
+
+
+router.post('/', async(req,res) => {
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price
+    })
+
+    try{
+        const a1 =  await product.save() 
+        res.json(a1)
+    }catch(err){
+        res.send('Error')
     }
 })
 
